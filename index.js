7de01512867647b23c1d1c68a6e2d374f44e9456
Laura Felce <crepu83@yahoo.it>
2018-09-18 09:04:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ return (`Welcome, ${name}. You are number ${position.length} in line.`);
 
 
 
+
 function nowServing (position) {
 position.shift(`${name}`);
- console.log (`Currently serving ${name}`);
+ return (`Currently serving ${name}`);
   if (position.length === 0) {
   return (`There is nobody waiting to be served!`)}
   }
