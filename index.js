b78b62a5ca8e372749df7b153002106b1da557f6
Laura Felce <crepu83@yahoo.it>
2018-09-18 07:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber (position, name) {
+  var otherDeli = []
   for (let i = 1; i < name.length; i++) {
-array.push(`Welcome, ${name[i]}. You are number ${position[i]}in the line.`);}
-return array;
+otherDeli.push(`Welcome, ${name[i]}. You are number ${position[i]}in the line.`);}
+return otherDeli;
 }
