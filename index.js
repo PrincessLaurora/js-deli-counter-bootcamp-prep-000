02580eeadba4eb03a7bfbd9f35fd9421eae98256
Laura Felce <crepu83@yahoo.it>
2018-09-18 08:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ return (`Welcome, ${name}. You are number ${position.length} in line.`);
 }
 
 function nowServing (position) {
-  
+  if position.shift(`${name}`);
+  return (`Now serving ${name}`);
+  else 
 }
