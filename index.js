6ced5ef714fd91925862348781668c8323ef663e
Laura Felce <crepu83@yahoo.it>
2018-09-18 10:52:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function nowServing (position) {
 function currentLine(line) {
  if (line.length === 0) {
    return (`The line is currently empty.`)}
- var currentLine = []
- for (let i = 0; i>line.length, i++){
-   
+ var currentLine = [];
+ for (let i = 0; i>line.length; i++){
+   currentLine.push(`${i+1}. $line[1}}`);
  }
- return (`The line is currently ${line.length}. .`);
+ return (`The line is currently: + currentLine`);
  
 }
