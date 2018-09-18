d80e2688a3fe8811a250f604759815e012945495
Laura Felce <crepu83@yahoo.it>
2018-09-18 07:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (position, name) {
   for (let i = 1; i < name.length; i++) {
 array.push(`Welcome, ${name[i]}. You are number ${position[i]}in the line.`);}
-}
 return array;
+}
