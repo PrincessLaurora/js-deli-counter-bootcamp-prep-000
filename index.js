be200f566a1ee83cf411388189c4974363bcfb07
Laura Felce <crepu83@yahoo.it>
2018-09-18 10:54:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ function nowServing (position) {
 function currentLine(line) {
  if (line.length === 0) {
    return (`The line is currently empty.`)}
+   else {
  var currentLine = [];
  for (let i = 0; i>line.length; i++){
    currentLine.push(`${i+1}.${line[1]}`);
  }
  return (`The line is currently: + currentLine`);
- 
+   }
 }
